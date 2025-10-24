a542122b3fbfa45a79412be8c827ece7258bc5d7
Itch-hash <contact.itch8@gmail.com>
2025-10-24 04:32:59 +0300

    resolved a bug where recipes.json was not bundled correctly and moved it from static to lib

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,8 +1,13 @@
 //All recipes are fetched here then exported to other components
 /** @type {import('./$types').PageLoad} */
 
-export async function load({ fetch }) {
-	const res = await fetch('/recipe.json');
-	const recipes = await res.json();
+// export async function load({ fetch }) {
+// 	const res = await fetch('/recipe.json');
+// 	const recipes = await res.json();
+// 	return { recipes };
+// }
+import recipes from '$lib/data/recipe.json' assert { type: 'json' };
+
+export function load() {
 	return { recipes };
 }
